b1d599f7c096aad09a1a2765c1b660be8253a3d0
Maximiliano Gartner <maxi-gartner@hotmail.com>
2023-04-24 20:36:10 -0300

    F-03

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,11 @@
-import apiUrl from "../api"
-import Navbar from "./components/Navbar"
-import Footer from "./components/Footer"
-import Main from "./components/Main"
+import Index from "./components/Index"
+import Main from "./layouts/main"
 
 function App() {
-  console.log(apiUrl)
-
   return (
-    <div className='xl:mx-24 sm:mx-0 bg-black text-white sm:text-black sm:bg-white'>
-    <nav>
-      <Navbar/>
-    </nav>
-    <main>
-      <Main/>
-    </main>
-    <footer>
-        <Footer/>
-    </footer>
-</div>
+      <Main>
+          <Index/>
+      </Main>
   )
 }
 
