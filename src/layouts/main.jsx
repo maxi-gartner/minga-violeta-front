8ec08fbfdb4c83688e0f9ae6b835823d595f8ef3
Denise Aguirre M <deniseaguirre.m@gmail.com>
2023-05-02 15:40:12 -0300

    restart repo

--- src/layouts/main.jsx
+++ src/layouts/main.jsx
@@ -1,14 +1,15 @@
 import Navbar from "../components/Navbar"
 import Footer from "../components/Footer"
 import Dravel from "../components/Dravel"
+import { Outlet } from "react-router-dom"
 
-export default function main({ children }){
+export default function main(){
     return (
         <>
             <div className='h-full sm:mx-0 lg:mx-24 xl:mx-52 bg-black text-white sm:text-black sm:bg-white'>
                 <Dravel/>
                 <Navbar/>
-                {children}
+                    <Outlet />
                 <Footer/>
             </div>
         </>
