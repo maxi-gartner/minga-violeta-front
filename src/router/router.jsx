9145226d2240cf7fadb7403b44f363d2f33b73cf
JuanFrancoRod <127918947+JuanFrancoRod@users.noreply.github.com>
2023-05-03 12:25:43 -0300

    m-01 cambios (#15)
    Co-authored-by: JuanFrancoRodriguez <juanfrancorod@gmai.com>

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -2,14 +2,19 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../App.jsx"
 import LayoutMain from "../layouts/Main.jsx"
 import AuthorRegMain from "../pages/authorRegMain.jsx"
+import SignUp from "../pages/signUp.jsx";
+import SignIn from "../pages/signIn.jsx";
 import MangaForm from "../pages/MangaForm.jsx"
 
 const routes = createBrowserRouter([
     { path: '/', element:<LayoutMain/>, children:[
         {path: '/', element:<Main/>},
         {path: '/AuthorRegister/:page', element: <AuthorRegMain/>},
-        {path: '/manga-form', element: <MangaForm/>}
+        {path: '/manga-form', element: <MangaForm/>},
+         { path: "/auth/signup", element: <SignUp /> },
+      { path: "/auth/signin", element: <SignIn /> },
     ]}
 ])
+
 
 export default routes
