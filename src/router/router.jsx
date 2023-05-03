74cb359afb7013b5abe5d5c106ac3312017ea629
Denise Aguirre Martinez <31725325+DeniseAguirre@users.noreply.github.com>
2023-05-03 19:33:22 -0300

    M 04 02 (#16)
    * change dravel, authorRegMain, router, main
    
    * Add modal Chapter
    
    ---------
    
    Co-authored-by: Denise Aguirre M <deniseaguirre.m@gmail.com>

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -5,16 +5,17 @@ import AuthorRegMain from "../pages/authorRegMain.jsx"
 import SignUp from "../pages/signUp.jsx";
 import SignIn from "../pages/signIn.jsx";
 import MangaForm from "../pages/MangaForm.jsx"
+import ChapterForm from "../pages/ChapterForm.jsx"
+
 
 const routes = createBrowserRouter([
     { path: '/', element:<LayoutMain/>, children:[
         {path: '/', element:<Main/>},
         {path: '/AuthorRegister/:page', element: <AuthorRegMain/>},
         {path: '/manga-form', element: <MangaForm/>},
-         { path: "/auth/signup", element: <SignUp /> },
-      { path: "/auth/signin", element: <SignIn /> },
+        {path: "/auth/signup", element: <SignUp />},
+        {path: "/auth/signin", element: <SignIn />},
+        {path: '/chapter-form/:id_manga', element:<ChapterForm/>}
     ]}
 ])
-
-
 export default routes
