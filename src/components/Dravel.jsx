51c18df532e1c5b47b83fe1724796d4b05047de4
Maximiliano Gartner <113448691+maxi-gartner@users.noreply.github.com>
2023-05-04 13:41:25 -0300

    M 02 (#22)
    * create page company form
    
    * m-02-4/5
    
    * clear user localStorage
    
    * m-02
    
    * m-02

--- src/components/Dravel.jsx
+++ src/components/Dravel.jsx
@@ -1,18 +1,19 @@
-let role = localStorage.getItem('role')
-let email = localStorage.getItem('email')
-// let email = localStorage.getItem('email')
-
 export default function Dravel() {
+    let role = localStorage.getItem('role') ;
+    let email = localStorage.getItem('email');
+    let photo = localStorage.getItem('photo');
+
     let authors = "none"
     let mangas = "none"
     let company = "none"
-    
+
     const viewsButtons = () => {
-        if(role === 0){
+        console.log(role)
+        if(role == 0){
             authors = "flex"
             company = "flex"
         }
-        if(role === 1 || role === 2){
+        if(role == 1 || role === 2){
             mangas = "flex"
         }
     }
@@ -24,9 +25,9 @@ export default function Dravel() {
             <div className="hidden flex-col h-screen w-screen sm:w-[400px] bg-gradient-to-b from-[#F9A8D4] to-[#F472B6] z-50 fixed left-0" id='dravel'  /* style={{display:stateDisplay[counter]}} */>
                 <div className="w-full p-5 sm:text-xl relative text-white flex">
                     <div className="w-14 h-14 mr-5 rounded-full overflow-hidden  shadow-[0px_0px_40px_4px_rgba(0,0,0,0.56)]">
-                        <img src="../Logo.png" alt="" />
+                        <img className="object-cover h-full" src={photo} alt="" />
                     </div>
-                    <div>
+                    <div className="h-14 flex items-center">
                         <p>{email}</p>
                         <svg onClick={subtract} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"  stroke="currentColor" className="w-10 h-10 absolute right-3 top-3  cursor-pointer"><path d="M6 18L18 6M6 6l12 12" /></svg>
                     </div>
