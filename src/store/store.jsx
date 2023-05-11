19d0570e48db19347f28be0102e2c358d634c508
Denise Aguirre Martinez <31725325+DeniseAguirre@users.noreply.github.com>
2023-05-11 19:06:57 -0300

    M 09 (#31)
    * error navigate
    
    * add store and view front
    
    * fixed signup and signin
    
    * add useLocation in navbar
    
    ---------
    
    Co-authored-by: Denise Aguirre M <deniseaguirre.m@gmail.com>

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,3 +1,12 @@
+<<<<<<< HEAD
+import { configureStore } from "@reduxjs/toolkit"
+import chapter_reducer from './reducers/chapter_bar'
+
+const store = configureStore({ 
+    reducer: {
+        data: chapter_reducer
+    }
+=======
 import { configureStore } from "@reduxjs/toolkit";
 import mangasReducer from './reducers/mangasFilter'
 import savePageReducer from './reducers/saveCurrentPage'
@@ -9,6 +18,7 @@ const store = configureStore({
                 currentPage: savePageReducer,
                 data: chapter_reducer
         }
+>>>>>>> 458e1f0f4ecc804a517f3adb4c89dd910da227d1
 })
 
 export default store
