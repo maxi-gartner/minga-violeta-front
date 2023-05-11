90130986777723c2bb602ea774c5995831a78085
Tomas Salazar <tomas98salazar@gmail.com>
2023-05-11 14:28:12 -0300

    m-07 s3

--- src/pages/Mangas.jsx
+++ src/pages/Mangas.jsx
@@ -1,49 +1,64 @@
 import axios from "axios"
 import apiUrl from "../../api"
 import { useState, useEffect, useRef } from "react"
-import { Link, useParams } from "react-router-dom"
+import { useSelector, useDispatch } from "react-redux"
+import inputs_filter_actions from '../store/actions/inputs_filter'
+import { Link as Anchor } from "react-router-dom"
 
 export default function Mangas() {
+    const buscador = useRef('')
+    const category_id = useRef('')
+    const nextP = useRef()
+    const prevP = useRef()
+    const {title, categories} = useSelector( store => store.inputs)
+    const dispatch = useDispatch()
+    const [mangas, setMangas] = useState()
+    const [page, setPage] = useState(1)
+    const [limit, setLimit] = useState(6)
+    const [reload, setReload] = useState(false)
+    const [categorias, setCategorias] = useState([])
     let token = localStorage.getItem('token')
     let headers = {headers:{'Authorization' : `Bearer ${token}`}}
-    const title = useRef()
-    const category_id = useRef()
-    const [mangas, setMangas] = useState()
-    const [reload, setReload] = useState(false)
-    const [categories, setCategories] = useState([])
-
+    const {inputs_filter} = inputs_filter_actions
+    console.log(categories)
     useEffect(
         ()=>{
-
-            let categories = Object.values(category_id.current)
-
-            let values = []
-            categories.forEach(each=>{
-                if(each.checked){
-                    values.push(each.value)
-                }
-            })
-            console.log(values)
-            axios(apiUrl+`mangas?title=${title.current.value}&category_id=${values.join(',')}&order=1&page=1&limit=6`, headers).then(res=>setMangas(res.data.response)).catch(err=>console.log(err))
+            axios(apiUrl+`mangas?title=${buscador.current.value}&category_id=${categories.join(',')}&order=1&page=${page}&limit=${limit}`, headers).then(res=>setMangas(res.data.response)).catch(err => console.log(err))
         },
         [reload]
     )
+    console.log("count", count)
+    function next() {
+        console.log(next)
+        setPage(page+1)
+        setReload(!reload)
+    }
+    function prev() {
+        setPage(page-1)
+        setReload(!reload)
+    }
+    console.log(mangas)
 
     useEffect(
-        ()=>{
-            axios(apiUrl+'categories', headers).then(res=>setCategories(res.data.categories)).catch(err=>console.log(err))
+        () => {
+            axios(apiUrl + 'categories', headers).then(res => setCategorias(res.data.categories)).catch(err => console.log(err))
         },
         []
     )
-    const checkbox = ()=> {
-        const array = categories.map(category => ({
-            id: category._id,
-            name: category.name
-        }));
-        console.log(array);
+    
+    function capture(){
+        dispatch(inputs_filter({
+            title: buscador.current?.value,
+            categories: Object.values(category_id.current)
+                .filter(each => each.checked)
+                .map(each => each.value)
+        }))
         setReload(!reload)
+        setLimit(10)
+        console.log(categories)
+        console.log(title)
     }
-
+console.log(categories)
     return (
         <>
             <div className="w-full h-auto flex flex-col items-center">
@@ -56,7 +71,7 @@ export default function Mangas() {
                                 <path d="M30.8333 31.3333L26.2083 26.7083" stroke="#F472B6" strokeWidth="2" strokeLinecap="round" />
                             </svg>
                         </label>
-                        <input type="search" name="title" id="title" ref={title} onKeyUp={()=>setReload(!reload)} placeholder=" Find your manga here" className="h-full w-full rounded-full text-lg bg-white sm:bg-gray-200" />
+                        <input type="text" name="title" id="title" defaultValue={title} ref={buscador} onKeyUp={capture} placeholder=" Find your manga here" className="h-full w-full rounded-full text-lg bg-white sm:bg-gray-200 outline-none pl-2" />
                     </form>
                 </div>
                 <div className="w-full h-auto sm:h-1/3 sm:w-11/12 bg-gray-200 flex justify-center mb-16">
@@ -73,45 +88,33 @@ export default function Mangas() {
                                 <div className="w-full text-xs text-white font-normal flex justify-center">Popular</div>
                             </div>
                         </div>
-                        <form ref={category_id} className="w-full flex justify-start mt-4 sm:justify-start sm:pl-12 md:pl-16 lg:pl-20 xl:pl-24 2xl:pl-28 sm:gap-x-4">
-                            {categories && categories.map((category) =>
-                                <label htmlFor={category._id} key={category._id} style={{ backgroundColor: category.hover, color: category.color, padding: '0.25rem', borderRadius: '20px' }}>
-                                    {category.name}
-                                    <input name="category_id" onChange={checkbox} className={`hover:bg-${category.hover}`} style={{appearance: 'none'}} type="checkbox" value={category._id} id={category._id} />
+                        <form ref={category_id} className="w-full flex justify-center mt-4 sm:justify-center lg:justify-start lg:pl-20 xl:pl-24 2xl:pl-28 gap-x-2 sm:gap-x-4">
+                            {categorias && categorias.map((category) =>
+                                <label htmlFor={category._id} key={category._id} className="hover:shadow-lg hover:border-2 border-white" style={{ height: "2rem", backgroundColor: category.hover, color: category.color, padding: '0.3rem', borderRadius: '16px', fontSize: "12px", textAlign: "center", ...(categories.includes(category._id)? {backgroundColor: category.color, color: "white"} : {}) }}>
+                                    {category.name.charAt(0).toUpperCase() + category.name.slice(1)}
+                                    <input name="category_id" onClick={capture} defaultChecked={categories.includes(category._id)} style={{ appearance: 'none' }} type="checkbox" value={category._id} id={category._id} />
                                 </label>)}
                         </form>
-{/*                     <div className="w-full flex justify-between mt-4 sm:justify-start sm:pl-12 md:pl-16 lg:pl-20 xl:pl-24 2xl:pl-28">
-                            <div className="bg-gray-500 inline-block rounded-2xl invisible absolute sm:relative sm:visible sm:mr-4"><p className="text-white text-xs p-1">All</p></div>
-                            <div className="bg-red-200 inline-block rounded-2xl sm:mr-4"><p className="text-red-400 text-xs p-1">Shōnen</p></div>
-                            <div className="bg-orange-200 inline-block rounded-2xl sm:mr-4"><p className="text-orange-400 text-xs p-1">Seinen</p></div>
-                            <div className="bg-green-200 inline-block rounded-2xl sm:mr-4"><p className="text-green-400 text-xs p-1">Shōjo</p></div>
-                            <div className="bg-violet-200 inline-block rounded-2xl sm:mr-4"><p className="text-violet-400 text-xs p-1">Kodomo</p></div>
-                            <svg className="w-6 h-6 inline-block sm:hidden" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#F472B6">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M3 4.5h14.25M3 9h9.75M3 13.5h5.25m5.25-.75L17.25 9m0 0L21 12.75M17.25 9v12" />
-                            </svg>
-                        </div> */}
-                        {mangas?.map(manga =>
-                            <div key={manga._id} className="w-full h-[20vh] mt-4 sm:mt-6 flex justify-between bg-white rounded-lg shadow-md sm:w-[60%] lg:w-[40%]">
-                                <div className="w-1/2 h-full flex justify-center items-center">
-                                    <div className="w-full h-3/4 border-l-4 border-orange-400 flex flex-col justify-center">
-                                        <p className="text-xs font-semibold ml-1">{manga.title}</p>
-                                        <p className="text-xs font-semibold ml-1 text-orange-400">Type</p>
-                                        <div className="mt-24 ml-8 text-xs bg-green-200 invisible absolute sm:absolute sm:visible inline-block rounded-2xl"><p className="text-green-500 text-xs font-bold p-2">Read</p></div>
+                        {mangas && mangas.length > 0 ? (
+                            mangas?.map((manga =>
+                                <div key={manga._id} className="w-full h-[20vh] mt-4 sm:mt-6 flex justify-between bg-white rounded-lg shadow-md sm:w-[60%] lg:w-[40%]">
+                                    <div className="w-1/2 h-full flex justify-center items-center">
+                                        <div className="w-full h-3/4 border-l-4 flex flex-col justify-center" style={{ borderColor: manga.category_id.color }}>
+                                            <p className="text-xs xl:text-lg font-semibold ml-1">{manga.title}</p>
+                                            <p className="text-xs xl:text-md font-semibold ml-1" style={{ color: manga.category_id.color }}>{manga.category_id.name.charAt(0).toUpperCase() + manga.category_id.name.slice(1)}</p>
+                                            <div className="mt-24 ml-8 text-xs bg-green-200 invisible absolute sm:absolute sm:visible inline-block rounded-2xl hover:shadow-md hover:border-2"><Anchor to={"/detailsManga/" + manga._id}><p className="text-green-500 text-xs font-bold p-2 px-6">Read</p></Anchor></div>
+                                        </div>
                                     </div>
+                                    <img src={manga?.cover_photo} className="w-40 sm:w-60 object-cover h-full rounded-[40px_8px_8px_40px/64px_8px_8px_64px;]" alt="" />
                                 </div>
-                                <img src={manga?.cover_photo} className="w-40 sm:w-60 object-cover h-full rounded-[40px_8px_8px_40px/64px_8px_8px_64px;]" alt="" />
-                            </div>
+                            ))
+                        ) : (
+                            <div className="flex justify-center items-center w-full h-[30vh] mt-4"><p className="bg-black opacity-80 rounded-full p-1 lg:p-3 text-white text-center">No manga has been found</p></div>
                         )}
-                        {/* <div className="w-full h-[20vh] mt-4 sm:mt-6 flex justify-between bg-white rounded-lg shadow-md sm:w-[40%]">
-                            <div className="w-1/2 h-full flex justify-center items-center">
-                                <div className="w-full h-3/4 border-l-4 border-orange-400 flex flex-col justify-center">
-                                    <p className="text-xs font-semibold md:-">Naruto: And That's Why You're Disqualified!! #8</p>
-                                    <p className="text-xs font-semibold text-orange-400">Type</p>
-                                    <div className="mt-24 ml-8 text-xs bg-green-200 invisible absolute sm:absolute sm:visible inline-block rounded-2xl"><p className="text-green-500 text-xs font-bold p-2">Read</p></div>
-                                </div>
-                            </div>
-                            <img src="../../mangasFotito4.png" className="w-30 h-full rounded-[40px_8px_8px_40px/64px_8px_8px_64px;]" alt="" />
-                        </div> */}
+                        <div className="w-full flex justify-center pt-4 text-white">
+                            {page == 1 ? null : (<input className="bg-red-400 h-10 w-60 p-2 m-2 rounded-2xl bg-gradient-to-b from-[#F9A8D4] to-[#F472B6] font-semibold hover:shadow-lg hover:border-2" type="button" value="Prev" onClick={prev} ref={prevP}/>)}
+                            {page > 3 ? null : (<input type="button" className="bg-red-400 h-10 w-60 p-2 m-2 rounded-2xl bg-gradient-to-b from-[#F9A8D4] to-[#F472B6] font-semibold hover:shadow-lg hover:border-2" value="Next" onClick={next} ref={nextP}/>)}
+                        </div>
                     </div>
                 </div>
             </div>
