13b7b4c5215031e2d64b438d6979c85f903e7cf2
Maximiliano Gartner <maxi-gartner@hotmail.com>
2023-05-16 12:10:50 -0300

    repair signIn

---
+++ src/pages/MyMangas.jsx
@@ -0,0 +1,20 @@
+import axios from "axios"
+import { useEffect, useState } from "react";
+import apiUrl from "../../api"
+
+export default function MyMangas() {
+    const [manga, setManga] = useState()
+    useEffect(() => {
+        let token = localStorage.getItem('token')
+        let headers = { headers: { 'Authorization': `Bearer ${token}`} }
+        axios.get(apiUrl+"mangas/me", headers).then(res=> setManga(res.data.response)).catch(err => console.log(err))
+    }, []
+    )
+    console.log(manga);
+
+  return (
+    <div>
+      <p>Pagina my mangas</p>
+    </div>
+  );
+}
